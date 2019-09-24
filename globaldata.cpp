f9b944d483d3da6c536462fab446b9e25cad287a
nephen <995168694@qq.com>
2019-09-24 20:28:00 +0800

    更改初始化

--- globaldata.cpp
+++ globaldata.cpp
@@ -5,12 +5,11 @@ int GlobalData::connectType=NONE_CONNECT;
 QStringList GlobalData::currentCanData;
 bool GlobalData::showDebugInfo=false;
 int GlobalData::runningId[NODE_NUM]={0};
-unsigned int GlobalData::sendId[NODE_NUM] = {0x01, 0x02, 0x03, 0x04,
-                                             0x05, 0x06, 0x07, 0x08,
-                                             0x09, 0x0A, 0x0B, 0x0C}; //joint module
-//                                    0x0D,0x0F};//car wheel
+unsigned int GlobalData::sendId[NODE_NUM] = {0};
 
 GlobalData::GlobalData()
 {
-
+    for(int i=0;i<NODE_NUM;i++) {
+        sendId[i] = 0x01+static_cast<unsigned int>(i);
+    }
 }
